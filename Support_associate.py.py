6659cb0e0fd4870730091c23731c03454ebe2000
aishwaryaa-45 <hukkeriaishwarya9@gmail.com>
2025-11-17 19:10:04 +0530

    added

--- Support_associate.py.py
+++ Support_associate.py.py
@@ -7,7 +7,8 @@ from huggingface_hub import InferenceClient
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_community.document_loaders import TextLoader
-from langchain.text_splitter import CharacterTextSplitter
+from langchain_text_splitters import CharacterTextSplitter
+
 from langchain.chains import RetrievalQA
 from langchain_core.language_models import LLM
 from pydantic import PrivateAttr
